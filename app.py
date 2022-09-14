feb1ad11d9509c44812bc5188ac79cf3feb4c5a1
RohitKV1n0d <rohitvinod92@gmail.com>
2022-09-14 16:06:54 +0530

    add home

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
+from crypt import methods
 import flask
 from flask import jsonify
 app = flask.Flask(__name__)
 
-@app.route('/<int:a>/<int:b>')
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/<int:a>/<int:b>',methods=['GET','POST'])
 def add(a,b):
     sum = str(a+b)
     return jsonify({'sum': sum})
 
+
 if __name__ == '__main__':
     app.run()
-    
+
   
