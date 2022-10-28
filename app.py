4fac947f027159b8327a5c68e111de26bdb6a630
RohitKV1n0d <rohitvinod92@gmail.com>
2022-10-28 14:58:01 +0530

    new

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from crypt import methods
+from operator import concat
 import flask
-from flask import jsonify
+from flask import jsonify,request
 app = flask.Flask(__name__)
 
 
@@ -13,6 +14,11 @@ def add(a,b):
     sum = str(a+b)
     return jsonify({'sum': sum})
 
+@app.route('/sms/<int:id>',methods=['GET','POST'])
+def sms(id):
+    content = request.json
+    print(content)
+    return content
 
 if __name__ == '__main__':
     app.run()
