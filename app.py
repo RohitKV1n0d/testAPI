a30b5dedeefb67a8862fd90dae1cda518762af1b
RohitKV1n0d <rohitvinod92@gmail.com>
2022-10-29 11:58:46 +0530

    added getOTP route

--- app.py
+++ app.py
@@ -27,21 +27,21 @@ def add(a,b):
 def sms():
     if request.method == 'POST':
         content = request.json
-        msg = content["content"]
+        msg = str(content["content"])
         otp = msg[61:67]
         with open("otp.txt", "w") as outfile:
             outfile.write(otp)
         
         return 'success'
     
-    f = open("json.txt", "r")
+    f = open("otp.txt", "r")
 
     data = f.read()
     return "<h1> {}</h1>".format(data)
 
 @app.route('/getotp',methods=['GET','POST'])
 def getotp():
-    f = open("json.txt", "r")
+    f = open("otp.txt", "r")
 
     data = f.read()
 
